64c354f878fd0e892161694725ef1dfcff3674a0
groiq <35578027+groiq@users.noreply.github.com>
2018-10-16 10:29:01 +0200

    ics generator: set up test data

--- playground.py
+++ playground.py
@@ -1,2 +1,19 @@
 #!/usr/bin/env python3
 
+
+
+# set up test data for calendar generation
+
+calendarData = """
+---
+first meeting
+your office
+we will discuss stuff.
+---
+second meeting
+my office
+we will talk some more.
+---
+"""
+
+calendarData = calendarData.split("---")
