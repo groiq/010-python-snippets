b399481d0d3ce7ec46b3d03c99275daf1a81671c
groiq <35578027+groiq@users.noreply.github.com>
2018-10-16 10:50:17 +0200

    set up test calendar data: finished strings part

--- playground.py
+++ playground.py
@@ -5,20 +5,41 @@
 # set up test data for calendar generation
 
 calendarData = """
----
-first meeting
-your office
+//
+first meeting/
+your office/
 we will discuss stuff.
----
-second meeting
-my office
+//
+second meeting/
+my office/
 we will talk some more.
----
+//
 """
 
-# calendarData = calendarData.split("---")
+calendarData = calendarData.split("//")
 
+# remove empty items
+for item in calendarData:
+    if not item.rstrip():
+        calendarData.remove(item)
 
+for i in range(len(calendarData)):
+    # print(calendarData[i])
+    entryList = calendarData[i].split("/")
+    entry = dict()
+    entry["name"] = entryList[0].strip()
+    entry["location"] = entryList[1].strip()
+    entry["description"] = entryList[2].strip()
+    calendarData[i] = entry
+    
+
+        
+# cd2 = calendarData.split("---")
+# cd3 = list()
+# for item in cd2:
+    # cd3.append(item.split("\n"))
+
+# calendarData = cd3
 
 # infile = open("./privateData/infile.txt", "r", encoding="utf-8")
 # print(infile)
@@ -27,5 +48,5 @@ we will talk some more.
 # infile.close()
 
 outfile = open("./privateData/outfile.ics", "w", encoding="utf-8")
-outfile.write(calendarData)
+outfile.write(format(calendarData))
 outfile.close()
