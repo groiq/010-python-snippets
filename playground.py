5469186419e0151bc2b555176809c6d9fb16f49b
groiq <35578027+groiq@users.noreply.github.com>
2018-10-18 11:31:16 +0200

    create ics directly: setup

--- playground.py
+++ playground.py
@@ -2,18 +2,20 @@
 
 output = []
 
-infile = open("privateData/infile.txt", "r", encoding="utf-8")
-for line in infile:
-    line = line.rstrip()
-    output.append(line)
-infile.close()
+# infile = open("privateData/infile.txt", "r", encoding="utf-8")
+# for line in infile:
+    # line = line.rstrip()
+    # output.append(line)
+# infile.close()
 
 
 
 
 
 outfile = open("privateData/outfile.ics", "w", encoding="utf-8")
+
 for line in output:
     outfile.write(format(line))
     outfile.write("\n")
+    
 outfile.close()
