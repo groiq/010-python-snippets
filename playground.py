a3e52640eef11642422c77123cccd6ec02ae8b4f
groiq <35578027+groiq@users.noreply.github.com>
2018-10-18 12:06:47 +0200

    rollback

--- playground.py
+++ playground.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
+import ics
+
+from pprint import pprint
+
 output = []
 
-# infile = open("privateData/infile.txt", "r", encoding="utf-8")
-# for line in infile:
-    # line = line.rstrip()
-    # output.append(line)
-# infile.close()
+infile = open("privateData/infile.txt", "r", encoding="utf-8")
+for line in infile:
+    line = line.rstrip()
+    output.append(line)
+infile.close()
+
+
+icsData = ics.Calendar()
 
 
 
-
+# write output
+# ------------
 
 outfile = open("privateData/outfile.ics", "w", encoding="utf-8")
 
