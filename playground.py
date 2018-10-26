208811e023923f558ce6d576b2f6cd372e05d3de
groiq <35578027+groiq@users.noreply.github.com>
2018-10-26 18:48:09 +0200

    import data

--- playground.py
+++ playground.py
@@ -3,6 +3,22 @@
 from pprint import pprint
 import csv
 
-csvData = csv.DictReader(open("./privateData/uniteCSV.csv", "r", encoding="utf-8"))
+csvData = csv.DictReader(open("./privateData/uniteCSV.csv", "r", encoding="utf-8-sig"), delimiter=";")
+urlList = open("./privateData/uniteCSV.txt", "r", encoding="utf-8").readlines()
+# print(type(urlList))
+pprint(urlList)
 
-pprint(csvData)
+
+data = list()
+
+for row in csvData:
+    # print(row)
+    # print("------------------------------------------------------")
+    # for item in row:
+        # print(item)
+        # print(row[item])
+    # print(row["Name"])
+    # print(row["Berufe"])
+    data.append(row)
+    
+pprint(data)
