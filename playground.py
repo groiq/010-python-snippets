3bd69055b586f8b30a24178e315feed1188485e6
groiq <35578027+groiq@users.noreply.github.com>
2018-10-26 11:10:09 +0200

    combine csv and url - setup
    I have some data in csv. Each dataset corresponds with a url, which I have in plain text. So I want to create a JSON object that contains the data together with the urls.

--- playground.py
+++ playground.py
@@ -1,48 +1,5 @@
 #!/usr/bin/env python3
 
-import ics
-import re
-
 from pprint import pprint
+import csv
 
-output = []
-
-
-
-icsData = ics.Calendar()
-newEntry = None
-newEntryRegex = r"""Do\. (\d{2})\.\s?(\d{2})\.\s?(\d{4})\s?:\s*[„“]?([^„“]*)[„“]?\s*"""
-
-
-infile = open("privateData/infile.txt", "r", encoding="utf-8")
-for line in infile:
-    line = line.rstrip()
-    # output.append(line)
-    newEntryEval = re.match(newEntryRegex,line)
-    if newEntryEval:
-        output.append(line)
-        output.append(newEntryEval.groups())
-    
-    
-infile.close()
-
-
-
-icsData.events.add(newEntry)
-
-# for line in output:
-    # icsData.events.add(newEntry)
-    # newEntry = ics.Event()
-
-# write output
-# ------------
-
-outfile = open("privateData/outfile.ics", "w", encoding="utf-8")
-
-outfile.writelines(icsData)
-
-for line in output:
-    outfile.write(format(line))
-    outfile.write("\n")
-    
-outfile.close()
