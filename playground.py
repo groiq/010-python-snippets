a9f2ac3729e502ed4926fef025ef323ac27e6769
groiq <35578027+groiq@users.noreply.github.com>
2018-11-16 10:10:19 +0100

    more terminal tests

--- playground.py
+++ playground.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 
 infile = open("outfile.txt", "r", encoding="utf-8").read()
+magicNum = int(infile) + 1
 
+outfile = open("outfile.txt", "w", encoding="utf-8")
+
+outfile.write(format(magicNum))
