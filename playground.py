ea098bf7d5890f3ca29490c5a68de584d43177b8
groiq <35578027+groiq@users.noreply.github.com>
2018-11-16 21:58:47 +0100

    functions for writing a date

--- playground.py
+++ playground.py
@@ -2,9 +2,18 @@
 
 from pprint import pprint
 
-# data struct mockup for timelogs
-
+# data structure mockup for weekly timelogs
 
 timelog = list()
+
+day = 1
+def date(day):
+    return "12766-03-{}".format(day)
+def mon(date):
+    return date(day)
+def sun(day):
+    return date(day+6)
+
+
 
 pprint(timelog)
