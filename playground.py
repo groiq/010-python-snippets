2c26033ca35ea4a6a2d6ee377b629a5ab06616ca
groiq <35578027+groiq@users.noreply.github.com>
2018-11-16 21:51:32 +0100

    data structure mockup for weekly timelogs: setup

--- playground.py
+++ playground.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python3
 
-infile = open("outfile.txt", "r", encoding="utf-8").read()
-magicNum = int(infile) + 1
+# data struct mockup for timelogs
 
-outfile = open("outfile.txt", "w", encoding="utf-8")
-
-outfile.write(format(magicNum))
