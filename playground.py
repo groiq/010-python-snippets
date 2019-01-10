dd23dd142e25910e5ac1391090e5e0971d0e23df
groiq <35578027+groiq@users.noreply.github.com>
2019-01-10 11:08:16 +0100

    write to html file

--- playground.py
+++ playground.py
@@ -6,3 +6,37 @@ from pprint import pprint
 
 # read a table from a csv file and render it as an html table with empty fields
 
+# read data
+
+
+
+# write html output
+
+header = """<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>csv2table</title>
+  </head>
+  <body>
+
+"""
+
+footer = """
+
+  </body>
+</html>"""
+
+output = open("./publicTestData/csv2table.html","w",encoding="utf-8")
+
+output.write(header)
+
+output.write("Content goes here\n")
+
+
+
+output.write(footer)
+
+
+output.close()
